81a55f431f54ea175bb02d49ac1fc294e25ffa94
Qcc <qiancc4@gmail.com>
2024-03-08 20:55:17 -0800

    update url in credit, delete unused tests

--- src/Credits.jsx
+++ src/Credits.jsx
@@ -1,18 +1,21 @@
 import Nav from "./components/nav";
 
-export default function Credits () {
-    return (
-        <div>
-            <Nav />
-            <div className="flex flex-col justify-center items-center">
-                <div className="text-2xl text-slate-800 font-bold m-5">Credits information</div>
-                <div className="flex flex-col justify-start items-start gap-5 m-10">
-                    <div>Developer: Chenyi Qian, Lilan Wu</div>
-                    <div>Github Repo: https://github.com/Qcc-2000/cs5610-project2</div>
-                </div>
-            </div>
-
+export default function Credits() {
+  return (
+    <div>
+      <Nav />
+      <div className="flex flex-col justify-center items-center">
+        <div className="text-2xl text-slate-800 font-bold m-5">
+          Credits information
         </div>
-        
-    );
+        <div className="flex flex-col justify-start items-start gap-5 m-10">
+          <div>Developer: Chenyi Qian, Lilan Wu</div>
+          <div>
+            Github Repo:
+            https://github.com/Qcc-2000/chenyi-qian-lilan-wu-assignment2
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
