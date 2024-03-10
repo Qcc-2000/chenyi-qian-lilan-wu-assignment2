1eefd346a85c96065a48d1157e90e27626208688
Qcc <qiancc4@gmail.com>
2024-03-09 16:16:36 -0800

    Update navigation styling in Nav component

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -5,30 +5,39 @@ export default function Nav() {
   return (
     <div className="sticky inset-x-0 top-0 z-30 w-full">
       <MaxWidth>
-        <div className="flex justify-between items-center h-10 border-b border-gray-200">
+        <div className="flex justify-between items-center h-10 border-b border-gray-200 backdrop-blur-md">
           <div className="flex items-center">
             <div className="text-2xl font-bold text-red-900">
               Conway’s Game of Life
             </div>
           </div>
           <div className="flex items-center gap-3 text-lg">
-            <NavLink to="/" className={({ isActive }) => {
-              return isActive ? 'text-red-900 font-bold' : ''
-            }}>
+            <NavLink
+              to="/"
+              className={({ isActive }) => {
+                return isActive ? "text-red-900 font-bold" : "";
+              }}
+            >
               Home
             </NavLink>
             <Outlet />
-            <NavLink to="/game" className={({ isActive }) => {
-              return isActive ? 'text-red-900 font-bold' : ''
-            }}>
+            <NavLink
+              to="/game"
+              className={({ isActive }) => {
+                return isActive ? "text-red-900 font-bold" : "";
+              }}
+            >
               Game
             </NavLink>
             <Outlet />
-            <NavLink to="/credits" className={({ isActive }) => {
-              return isActive ? 'text-red-900 font-bold' : ''
-            }}>
+            <NavLink
+              to="/credits"
+              className={({ isActive }) => {
+                return isActive ? "text-red-900 font-bold" : "";
+              }}
+            >
               Credits
-              </NavLink>
+            </NavLink>
             <Outlet />
           </div>
         </div>
